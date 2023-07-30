db6c91ca195df90adde2ffe269af7c75c4e4f09d
chenmoonmo <chenmoonmo@gmail.com>
2023-07-30 23:25:37 +0800

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,127 @@
-import Image from 'next/image'
+"use client";
+
+import React from "react";
+import { useState } from "react";
+import { toPng } from "html-to-image";
+import { saveAs } from "file-saver";
 
 export default function Home() {
+  const posterRef = React.useRef<HTMLDivElement>(null);
+  const bingoNames = [
+    "瀑布",
+    "搭档",
+    "作画",
+    "口罩+眼镜+帽子+耳机",
+    "古代人",
+    "谐音梗",
+    "厉害折扣",
+    "花束",
+    "跑者",
+    "落单耳机",
+    "摆拍",
+    "野猫对视",
+    "",
+    "球体",
+    "一饮而尽",
+    "好快的车",
+    "电量告急",
+    "排什么呢",
+    "哼歌",
+    "拍手",
+    "基本一致",
+    "燃起来了",
+    "淋雨",
+    "好招牌",
+    "好高的老人",
+  ];
+
+  const [bingoImages, setBingoImages] = useState<string[]>([]);
+
+  const handleImageChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    // set base64 image to bingoImages
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const base64 = e.target?.result;
+        if (typeof base64 === "string") {
+          setBingoImages((prev) => {
+            const next = [...prev];
+            next[index] = base64;
+            return next;
+          });
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleDownload = () => {
+    toPng(posterRef.current!, {
+      width: 511,
+      height: 661,
+    }).then(function (dataUrl) {
+      if (dataUrl) {
+        console.log(dataUrl);
+        saveAs(dataUrl, "MYNiCETRYBINGO.png");
+      }
+    });
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col items-center mt-4">
+      <div
+        ref={posterRef}
+        className="w-[511px] h-[661px] aspect-[255/330] p-[30px] pt-[83px] bg-[url('/bingo.jpeg')] bg-contain"
+      >
+        <div className="grid grid-rows-[repeat(5,83px)] grid-cols-[repeat(5,83px)] gap-[9px]">
+          {bingoNames.map((bingo, index) => {
+            const currentImage = bingoImages[index];
+            return (
+              <label
+                key={"bingo" + bingo}
+                htmlFor={"bingo" + bingo}
+                className="relative flex items-end justify-center cursor-pointer bg-cover bg-center bg-no-repeat"
+                style={{
+                  backgroundImage: `url(${bingoImages[index]})`,
+                  backgroundColor: currentImage ? "#FFF" : "transparent",
+                }}
+              >
+                <input
+                  hidden
+                  id={"bingo" + bingo}
+                  type="file"
+                  accept="image/*"
+                  onChange={(e) => handleImageChange(e, index)}
+                />
+
+                {currentImage && (
+                  <>
+                    <div
+                      className={`text-center font-medium pb-1 mix-blend-difference text-white whitespace-nowrap
+                 ${index === 3 ? "scale-50" : ""}
+                 `}
+                    >
+                      {bingo}
+                    </div>
+                    <div className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[105%] h-[105%] rounded-full border-[#EF3323] border-[3px] opacity-70" />
+                  </>
+                )}
+              </label>
+            );
+          })}
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <div className="text-white mt-1">点击对应图片上传，完成你的 #NiCE TRY BINGO！</div>
+      <button
+        className="text-3xl text-[#EF3323] font-bold bg-white px-10 py-5 mt-4"
+        onClick={handleDownload}
+      >
+        BINGO!
+      </button>
     </main>
-  )
+  );
 }
