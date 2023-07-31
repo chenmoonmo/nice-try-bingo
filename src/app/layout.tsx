ef9d323cbcccee051db138c63f0ed7be75929a24
chenmoonmo <chenmoonmo@gmail.com>
2023-07-31 10:33:29 +0800

    fix: 以轻面

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,34 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Noto_Sans_JP } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Noto_Sans_JP } from "next/font/google";
 
-const notoSansJP = Noto_Sans_JP({ subsets: ['latin'] })
+const notoSansJP = Noto_Sans_JP({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: '#NiCE TRY BINGO',
-  description: 'NiCE TRY 都市散步 BINGO，以轻松愉快的心情面对就可以了！',
-  icons: '/favicon.jpg'
-}
+  title: "#NiCE TRY BINGO",
+  description: "NiCE TRY 都市散步 BINGO，以轻松愉快的心情面对就可以了！",
+  icons: "/favicon.jpg",
+  twitter: {
+    card: "summary_large_image",
+    title: "#NiCE TRY BINGO",
+    description: "NiCE TRY 都市散步 BINGO，以轻松愉快的心情面对就可以了！",
+    images: "/bingo.jpeg",
+  },
+  openGraph: {
+    type: "website",
+    title: "#NiCE TRY BINGO",
+    description: "NiCE TRY 都市散步 BINGO，以轻松愉快的心情面对就可以了！",
+    images: "/bingo.jpeg",
+  },
+};
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={notoSansJP.className}>{children}</body>
     </html>
-  )
+  );
 }
