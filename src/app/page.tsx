73d7e7935a0c41851fea54c4e7460bea026e7931
chenmoonmo <chenmoonmo@gmail.com>
2023-07-31 13:41:12 +0800

    fix: 以轻面

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,60 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { toPng } from "html-to-image";
 import { saveAs } from "file-saver";
 
+const bingoNames = [
+  "瀑布",
+  "搭档",
+  "作画",
+  "口罩+眼镜+帽子+耳机",
+  "古代人",
+  "谐音梗",
+  "厉害折扣",
+  "花束",
+  "跑者",
+  "落单耳机",
+  "摆拍",
+  "野猫对视",
+  "",
+  "球体",
+  "一饮而尽",
+  "好快的车",
+  "电量告急",
+  "排什么呢",
+  "哼歌",
+  "拍手",
+  "基本一致",
+  "燃起来了",
+  "淋雨",
+  "好招牌",
+  "好高的老人",
+];
+
+const words = [
+  "我的HAPPY HOUR 就是现在",
+  "大风吹来了小猫",
+  "狠狠当一回大人",
+  "喜欢什么就一定要说出来",
+  "以轻松愉快的心情面对就可以了",
+  "谁是 NiCE TRY 最好笑的人",
+  "装个可爱混过去",
+  "来，对齐一下",
+  "内容太空",
+  "开心的时候听音乐，伤心的时候听歌词",
+  "#nicetry展",
+  "#nicetrybingo",
+];
+
 export default function Home() {
   const posterRef = React.useRef<HTMLDivElement>(null);
-  const bingoNames = [
-    "瀑布",
-    "搭档",
-    "作画",
-    "口罩+眼镜+帽子+耳机",
-    "古代人",
-    "谐音梗",
-    "厉害折扣",
-    "花束",
-    "跑者",
-    "落单耳机",
-    "摆拍",
-    "野猫对视",
-    "",
-    "球体",
-    "一饮而尽",
-    "好快的车",
-    "电量告急",
-    "排什么呢",
-    "哼歌",
-    "拍手",
-    "基本一致",
-    "燃起来了",
-    "淋雨",
-    "好招牌",
-    "好高的老人",
-  ];
-
   const [bingoImages, setBingoImages] = useState<string[]>([]);
+
+  const [isMounted, setIsMounted] = useState(false);
+
+  const wordsList = words.sort(() => Math.random() - 0.5);
 
   const handleImageChange = (
     e: React.ChangeEvent<HTMLInputElement>,
@@ -68,12 +87,37 @@ export default function Home() {
     });
   };
 
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
-    <main className="flex flex-col items-center p-4 pt-8">
+    <main className="relative flex flex-col items-center min-h-screen p-4 pt-8 overflow-hidden">
+      {isMounted &&
+        wordsList.map((word, index) => {
+          const left =
+            Math.random() > 0.5
+              ? Math.random() * 10 + 5
+              : Math.random() * -50 + 100;
+          return (
+            <a
+              key={index}
+              href="https://nicetrypod.com/"
+              target="__blank"
+              className={`absolute hidden right-0 text-4xl font-bold text-white -z-0 whitespace-nowrap opacity-60 hover:opacity-100 md:block cursor-[url("/try.png"),default]`}
+              style={{
+                top: `${(index + 1) * 60}px`,
+                left: `${left}%`,
+                animationDelay: Math.random() * 2 + "s",
+              }}
+            >
+              {word}
+            </a>
+          );
+        })}
       <div
         ref={posterRef}
-        // bg-[url('/bingo.jpeg')]
-        className="w-full max-w-[511px] aspect-[255/330] px-8 pt-5  bg-white bg-contain md:pt-7"
+        className="relative w-full max-w-[511px] aspect-[255/330] px-8 pt-5 bg-white bg-contain md:pt-7 z-100"
       >
         <h1 className="font-extrabold text-center underline decoration-[#FA0101] underline-offset-8 indent-6 md:text-xl">
           NiCE TRY 都市散步 BINGO
@@ -105,8 +149,12 @@ export default function Home() {
                 {hasBingo && (
                   <>
                     <div
-                      className={`text-xs text-center font-medium pb-1 mix-blend-difference text-white whitespace-nowrap
-                 ${index === 3 ? "scale-50" : ""} md:text-base
+                      className={`text-xs text-center font-medium pb-1 mix-blend-difference text-white whitespace-nowrap md:text-base
+                 ${
+                   index === 3
+                     ? "scale-50 md:scale-50"
+                     : "scale-90 md:scale-100"
+                 }
                  `}
                     >
                       {bingo}
@@ -136,7 +184,7 @@ export default function Home() {
         </div>
       </div>
       <button
-        className="text-3xl text-[#EF3323] font-bold bg-white px-10 py-5 mt-4"
+        className="text-3xl text-[#EF3323] font-bold bg-white px-10 py-5 mt-4 active:scale-90 active:shadow-xl transition-all"
         onClick={handleDownload}
       >
         BINGO!
